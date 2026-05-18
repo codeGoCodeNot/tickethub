2c5bfe2dc1cd41aefd7ed88d935ed3e7c03fec4c
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-05-18 09:20:47 +0800

    refactor ticket edit fetcher to not separate to ticket edit page

--- src/app/tickets/[ticketId]/edit/page.tsx
+++ src/app/tickets/[ticketId]/edit/page.tsx
@@ -1,10 +1,39 @@
+import CardCompact from "@/components/card-compact";
 import Heading from "@/components/heading";
 import Spinner from "@/components/spinner";
-import TicketEditFetcher from "@/features/ticket/components/ticket-edit-fetcher";
+import { getAuth } from "@/features/auth/queries/get-auth";
+import isOwner from "@/features/auth/utils/is-owner";
+import TicketUpsertForm from "@/features/ticket/components/ticket-upsert-form";
+import getTicket from "@/features/ticket/queries/get-ticket";
+import { notFound, forbidden } from "next/navigation";
 import { Suspense } from "react";
 
 type TicketEditPageProps = {
   params: Promise<{ ticketId: string }>;
+};
+
+const TicketEditFetcher = async ({ params }: TicketEditPageProps) => {
+  const { ticketId } = await params;
+  const ticket = await getTicket(ticketId);
+  const user = await getAuth();
+
+  if (!ticket) return notFound();
+
+  const isTicketOwner = isOwner(user, ticket);
+
+  if (!isTicketOwner) return forbidden();
+
+  return (
+    <div className="flex flex-col flex-1 items-center">
+      <CardCompact
+        title="Edit Ticket"
+        description="Edit an existing ticket."
+        content={
+          <TicketUpsertForm ticket={ticket} key={ticket.updatedAt.toString()} />
+        }
+      />
+    </div>
+  );
 };
 
 const TicketEditPage = ({ params }: TicketEditPageProps) => {
