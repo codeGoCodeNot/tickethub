6701fa463472b68a558dd7c2a6c9901075880303
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-05-19 07:21:53 +0800

    api route for tickets ticket implemented

---
+++ src/app/api/tickets/route.ts
@@ -0,0 +1,20 @@
+import getTickets from "@/features/ticket/queries/get-tickets";
+import { searchParamsCache } from "@/features/ticket/search-params";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const untypedSearchParams = Object.fromEntries(searchParams);
+  const { search, sort, page, size } =
+    searchParamsCache.parse(untypedSearchParams);
+
+  const { list, metadata } = await getTickets(
+    undefined,
+    search,
+    sort,
+    page,
+    size,
+  );
+
+  return Response.json({ list, metadata });
+}
